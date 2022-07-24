d4af330dff7ca89cc186a2d947fe533bf97d45b4
phamtien758 <phamtien758@gmail.com>
2022-07-24 14:20:57 +0700

    Update main.c

--- convert_string_to_int/main.c
+++ convert_string_to_int/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
 
+uint32_t strToInt(char* string)
+{
+    uint32_t val = 0;
+    uint8_t i=0;
+    while (string[i] != '\0') //1234
+    {
+        if((string[i]<48) || (string[i]>57))
+        {
+            printf("Chuoi khong hop le! Status code: ");
+            return -1;
+        }
+        ++i;
+    }
+    for(int j=1; j<=i; j++)
+    {
+        int temp;
+        temp = string[j-1] - 48;
+        val += temp * pow(10,i-j);
+    }
+    return val;
+}
+
+int main()
+{
+    char str[] = "1233456789"; // <- intput string here
+    uint32_t int_num = strToInt(str);
+    printf("%d", int_num);
+
+    return 0;
+}
