05cf4ea1997bac01eb8701eb8ada73e71bd07ff9
phamtien758 <phamtien758@gmail.com>
2022-07-26 23:29:52 +0700

    Create support_function.c

---
+++ strtod/support_function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+uint32_t strToInt(char* string)
+{
+    uint32_t val = 0;
+    uint8_t i=0;
+    while (string[i] != '\0') //1234
+    {
+        if((string[i]<48) || (string[i]>57))
+        {
+            printf("Chuoi khong hop le! Status code: ");
+            return -1;
+        }
+        ++i;
+    }
+    for(int j=1; j<=i; j++)
+    {
+        int temp;
+        temp = string[j-1] - 48;
+        val += temp * pow(10,i-j);
+    }
+    return val;
+}
